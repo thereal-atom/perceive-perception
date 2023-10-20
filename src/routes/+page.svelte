0cca67e07b3a8e7ec32e4534ee4327915268a175
thereal-atom <oscarfal2006@gmail.com>
2023-10-20 22:05:06 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<div class="flex flex-col items-center p-64">
+    <div class="flex flex-col">
+        <p class="text-2xl">Perception is unique. What one person finds interesting or beautiful, another finds boring or ugly. If everyone perceived everything in the exact same way, then it would be the same as no perception at all, and with no perception, nothing exists, because nothing can know about anything because they don't perceive it. It's not right to say that nothing exists, even though it does, but if nothing can experience anything, then what's the point of anything existing? If nothing experiences then nothing exists.</p>
+        <p class="text-2xl mt-8">So a humans ability to look at an object or an image or some words on a page and think different things and experience different emotions to every other human is truly unique and a beautiful aspect of life that should be appreciated.</p>
+        <p class="text-2xl mt-8">Photography is such a method to invoke thought and emotions and, in a way, a method to make the universe exist.</p>
+        <p class="text-2xl mt-8">
+            Looking at the following collection of photos,
+            every person will think and see and feel different things to next person, <br />
+            and every photographer who takes a photo of the same object will think and feel and see different things when taking that photo to the next photographer, <br />
+            and every person will think and see and feel different things to the ones they did the last time they experienced the image.
+        </p>
+        <p class="text-2xl mt-8">That's what makes life...</p>
+    </div>
+    <div class="flex flex-col mt-32">
+        <div class="flex flex-row items-center">
+            <img
+                class="w-[500px] rounded-xl"
+                src="/buildings.jpg"
+                alt="buildings"
+            />
+            <div class="flex flex-col">
+                <p class="text-3xl ml-32">This is a photo of some buildings. In the foreground, some small family homes, in the background some massive office and apartment buildings. The buildings are seemingly lined up in order of age, where the buildings closet to me are just being built, while the buildings at the back are massive and old, consumed by nature.</p>
+                <p class="text-3xl ml-32 mt-8">This is representative of the passage of life - the buildings are experiencing age. Even though they are built out of lifeless objects like bricks and windows and foundations and carpet, they are seemingly alive and getting older - just like the humans who built them.</p>
+                <p class="text-3xl ml-32 mt-8">This, perhaps, shows how humans not only perceive and experience and live, but they can pass on that ability to that which they perceive and experience.</p>
+                <p class="text-3xl ml-32 mt-8">The background buildings are also many times larger than the family homes in the foreground, perhaps indicative of humans' social nature. Individuals and small groups are powerless in comparison to a massive group of people.</p>
+            </div>
+        </div>
+        <div class="flex flex-row items-center">
+            <p class="text-xl text-justify">I wouldn't say this is a particularly impressive or though-provoking picture, as I think many in this collection are, but I really like it. I like the overgrown swamp vibes because it really brings us close to true, 'old and wise' nature. It's like a grand parent with so much knowledge to share with the world.</p>
+            <img
+                class="w-[600px] ml-32 rounded-md"
+                src="/swamp.jpg"
+                alt="swamp"
+            />
+        </div>
+    </div>
+</div>
